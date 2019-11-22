a4a7836e136a04efe23fc8dd545cb8455e54e636
K <kristina.i.bautista@gmail.com>
2019-11-22 12:29:15 -0500

    adding Vector Homie program file
    In this program, Vector drives off the charger and greets you with fist bump and some fun dialogue.

---
+++ VectorHomie.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import anki_vector
+
+# Create a Robot object
+robot = anki_vector.Robot()
+
+# Connect to the Robot
+robot.connect()
+
+# Vector Drives Off Charger, Says Greeting, and Fist Bumps
+with anki_vector.Robot() as robot:
+    robot.behavior.drive_off_charger()
+    robot.anim.play_animation_trigger('GreetAfterLongTime')
+    robot.behavior.say_text("Hey Homie, can I get a fistbump?")
+    robot.anim.play_animation('anim_fistbump_requestonce_02')
+    robot.behavior.say_text("Thanks, Homie, stay cool!")
+      
+# Disconnect from Vector
+robot.disconnect()
